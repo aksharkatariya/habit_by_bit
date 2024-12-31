1dff2dfc7c48e5e41e338a907173d7e431d42ebd
Akshar Katariya <77921452+aksharkatariya@users.noreply.github.com>
2024-12-31 05:33:11 +0000

    rails generate devise installed

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
 
   Rails.application.routes.draw do
     get("/backdoor", { :controller => "forms", :action => "add_form" })
-    get("/", { :controller => "forms", :action => "index" })
+    root to: "forms#index"
+    # get("/", { :controller => "forms", :action => "index" })
     post("/add_update", {:controller => "forms", :action => "add_task"})
   end
   
